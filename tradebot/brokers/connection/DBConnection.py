f527acc58daffb1cc05fed25a1c4de774a6fc259
GandhiParth <parthg1910@gmail.com>
2024-07-28 12:21:24 +0530

    adding dbname to credentials

--- tradebot/brokers/connection/DBConnection.py
+++ tradebot/brokers/connection/DBConnection.py
@@ -13,25 +13,26 @@ class DBConnection(BrokerConnection):
     Handles connection to different PostgreSQL DataBase
     """
 
-    def __init__(self, credentials_yaml_file: str, dbname: str) -> None:
-        """
-        dbname: datbase name to connect
-        """
+    def __init__(self, credentials_yaml_file: str) -> None:
         super().__init__(credentials_yaml_file)
         self.dbname = dbname
 
     def auto_login(self) -> object:
         try:
             conn = psycopg2.connect(
-                dbname=self.dbname,
+                dbname=self.credentials["dbname"],
                 user=self.credentials["user"],
                 password=self.credentials["password"],
                 host=self.credentials["host"],
                 port=self.credentials["port"],
             )
-            logger.info(f"Connected to database '{self.dbname}' successfully!")
+            logger.info(
+                f"""Connected to database `{self.credentials["dbname"]}` successfully!"""
+            )
             return conn
         except Exception as e:
-            logger.error(f"Unable to connect to database '{self.dbname}'")
+            logger.error(
+                f"""Unable to connect to database `{self.credentials["dbname"]}`"""
+            )
             logger.error(e)
             raise BrokerConnectionError(e) from e
